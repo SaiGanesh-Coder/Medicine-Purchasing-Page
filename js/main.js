df8fe29fefd5cab2849c0e24a9269fb23216f8b8
SaiGanesh-Coder <Saiganesh.Kudipudi@gmail.com>
2024-05-04 09:17:46 +0530

    index-changes

--- js/main.js
+++ js/main.js
@@ -405,19 +405,19 @@ let healthArticleList = [
         title: "All You Need To Know About Common Transmitted Diseases",
     },
     {
-        imgUrl: "https://blog-images.pharmeasy.in/2022/07/18013014/18.jpg?dim=1024x0",
+        imgUrl: "https://www.india.com/wp-content/uploads/2023/09/00000014-4.jpg",
         title: "Best Home Remedies For Thick Eyebrows",
     },
     {
-        imgUrl: "https://blog-images.pharmeasy.in/2022/07/18011028/6.jpg",
+        imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoQRl32j4jpAJtGBXMA-_XjzxccaewlZeDzw&usqp=CAU",
         title: "Natural Home Remedies To Reduce Belly Fat",
     },
     {
-        imgUrl: "https://blog-images.pharmeasy.in/2022/07/18005945/7.jpg",
+        imgUrl: "https://i.pinimg.com/736x/cd/05/fb/cd05fbec01dda4e23791e4bd35ba98b9.jpg",
         title: "Best Home Remedies for Oily Scalp and Hair",
     },
     {
-        imgUrl: "https://blog-images.pharmeasy.in/2021/01/03163336/shutterstock_1678501243-1-768x512.jpg",
+        imgUrl: "https://static.toiimg.com/thumb/msid-105032032/105032032.jpg?width=500&resizemode=4",
         title: "Turmeric: Uses, Benefits, Side Effects, and More!",
     },
 ];
